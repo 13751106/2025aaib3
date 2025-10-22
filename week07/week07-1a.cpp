00d5c374a8cbe86f6eb690a8bf4e7cc82326c568
13751106 <13751106@me.mcu.edu.tw>
2025-10-22 11:59:56 +0800

    Add files via upload

---
+++ week07/week07-1a.cpp
@@ -0,0 +1,27 @@
+/// week07-1a.cpp
+/// TAICA NYCU
+/// Input: 1-9 计 Output: 露伴よ
+/// ex: Input: 3
+/// 3 3 3 3 3
+/// 3 2 2 2 3
+/// 3 2 1 2 3
+/// 3 2 2 2 3
+/// 3 3 3 3 3 羆き糷加
+/// Input 2 Τ 3 糷加, Input 3 Τ 5 糷加, Input 5 Τ 9 糷加
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n; /// Step01: Input
+
+    for(int i=1; i<2*n; i++){ /// Step03: for癹伴, 加糷
+        for(int j=1; j<2*n; j++){ /// 材碭
+            if(j>1) cout << " "; /// Step05: ó繷 + ó碵
+            cout << n;
+        }
+        cout << "瞷琌材i加, i:" << i; /// Step04: 加糷芅琜(穦╊奔)
+        cout << endl; /// Step02: Output, 铬︽
+    }
+}
---
+++ week07/week07-2.cpp
@@ -0,0 +1,21 @@
+/// week07-2.cpp
+/// LeetCode 657. Robot Return to Origin
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x = 0, y = 0;
+        for ( char c : moves ) {
+            if ( c=='U') {
+                y++;
+            } else if ( c=='D') {
+                y--;
+            } else if ( c=='L') {
+                x--;
+            } else if ( c=='R') {
+                x++;
+            }
+        }
+        if(x==0 && y==0) return true;
+        else return false;
+    }
+};
